c17ce55307274f2561e63e8b634b233c8b0673f9
Breno Prata <brenoprata@hotmail.com>
2025-07-29 18:36:48 +0200

    feat: replace mocked data with FunTranslations API

--- io/repo/YodaTranslationRepo.ts
+++ io/repo/YodaTranslationRepo.ts
@@ -1,13 +1,19 @@
 class YodaTranslationRepo {
   async getTranslation(text: string) {
-    // const response = await fetch(
-    //   "https://api.funtranslations.com/translate/yoda.json",
-    //   { method: "POST", body: JSON.stringify({ text }) }
-    // );
-    //
-    // return response;
+    const response = await fetch(
+      "https://api.funtranslations.com/translate/yoda.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      }
+    );
 
-    const json = await import(
+    return response;
+
+    /*const json = await import(
       "../mocks/api.funtranslations.com_translate_yoda.json.json"
     );
 
@@ -15,7 +21,7 @@ class YodaTranslationRepo {
       json() {
         return json;
       },
-    });
+    });*/
   }
 }
 
