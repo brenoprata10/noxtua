964117501b76a7ae1fad40f8e93072a552eacd6e
Breno Prata <brenoprata@hotmail.com>
2025-07-29 18:36:16 +0200

    feat: add error handling to FunTranslations service

--- app/routes/translate.tsx
+++ app/routes/translate.tsx
@@ -3,26 +3,51 @@ import Content from "view/components/Content";
 import Sidepane from "view/components/Sidepane";
 import { createDefaultFunTranslationService } from "io/service/FunTranslationService";
 import { useActionData } from "react-router";
+import type { Translation } from "domain/types/Translation";
 
-export const action = async ({ request }) => {
-  const translationService = createDefaultFunTranslationService();
-  const translation = await translationService.getTranslation("placeholder");
-  // should I do something with that request?
+type TranslationActionPayload =
+  | { success: true; data: Translation }
+  | { success: false; error: string };
 
-  return translation;
+export const action = async ({
+  request,
+}): Promise<TranslationActionPayload> => {
+  try {
+    const translationService = createDefaultFunTranslationService();
+    const translation = await translationService.getTranslation(
+      "brave people deploy to production and go for lunch"
+    );
+    // should I do something with that request?
+
+    return { success: true, data: translation };
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      error.message;
+      return { success: false, error: error.message };
+    }
+  }
+
+  return {
+    success: false,
+    error: "Translation failed. Please contact support.",
+  };
 };
 
 export default function Translate() {
-  const translation = useActionData();
+  const translation = useActionData<typeof action>();
+  const output = translation?.success ? translation.data.text : "";
+  const errorMessage = !translation?.success ? translation?.error : "";
 
   return (
-    <div className="flex h-full py-3">
+    <div className="flex h-full">
       <title>New React Router App</title>
       <meta name="description" content="Welcome to React Router!" />
       <Sidepane>It would be nice to see past translations here.</Sidepane>
       <Content>
         <TranslateForm />
-        {JSON.stringify(translation)}
+        {output}
+        {errorMessage}
       </Content>
     </div>
   );
---
+++ domain/types/fun-translations/ErrorPayload.ts
@@ -0,0 +1,6 @@
+export type ErrorPayload = {
+  error: {
+    code: number;
+    message: string;
+  };
+};
