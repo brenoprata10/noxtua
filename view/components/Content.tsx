cabac804006d90786020627105fe581c6ca9be24
Breno Prata <brenoprata@hotmail.com>
2025-07-29 16:56:10 +0200

    fix: change color of text to black for Content component to improve readability

--- view/components/Content.tsx
+++ view/components/Content.tsx
@@ -1,6 +1,6 @@
 export default function Content({ children }) {
   return (
-    <div className="bg-zinc-50 max-w-xl  p-6 border border-zinc-100 w-full h-full rounded-lg">
+    <div className="bg-zinc-50 max-w-xl  p-6 border border-zinc-100 w-full h-full rounded-lg text-black">
       {children}
     </div>
   );
